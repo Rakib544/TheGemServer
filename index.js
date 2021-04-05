94ab166b7322584b0164adf945a19524a2caa490
Rakib544 <md.rakib10122003@gmail.com>
2021-04-06 00:14:31 +0600

    database connected

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjo8u.mongodb.net/${process.env.DB_NAME}retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
     res.send('Hello I am now working on TheGem Restaurant Project')
 })
 
-app.listen(8080)
+client.connect(err => {
+    const foodCollection = client.db(`${process.env.DB_NAME}`).collection("devices");
+    console.log('database connected')
+  });
+  
+  
+
+app.listen(process.env.PORT || 8080);
